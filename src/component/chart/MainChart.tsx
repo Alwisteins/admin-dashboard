6035b6fd59da170871f5cfc99b9c926b95631891
unknown <alwyyourbae@gmail.com>
2023-07-25 07:38:00 -0700

    finished main chart(i guest)

--- src/component/chart/MainChart.tsx
+++ src/component/chart/MainChart.tsx
@@ -20,12 +20,18 @@ const chartData = {
       backgroundColor: "#4B49AC",
       id: 1,
       label: "revenue",
-      data: [23, 14, 36, 39, 24, 38, 43, 36, 49, 52, 44],
+      data: [230, 235, 180, 390, 240, 200, 180, 140, 170, 230, 300],
     },
     {
       backgroundColor: "#F3797E",
       id: 2,
       label: "cost",
+      data: [210, 195, 215, 160, 175, 180, 160, 150, 130, 100, 160],
+    },
+    {
+      backgroundColor: "#7DA0FA",
+      id: 3,
+      label: "order",
       data: [48, 56, 54, 41, 36, 29, 30, 21, 18, 11, 21],
     },
   ],
@@ -33,8 +39,8 @@ const chartData = {
 
 export default function MainChart() {
   return (
-    <div className="absolute w-fit h-96 mx-40 mt-44 overflow-hidden">
-      <Chart type="bar" data={chartData} />
+    <div className="absolute w-fit h-96 mx-40 mt-44 hover:scale-105 duration-200 overflow-hidden">
+      <Chart type="bar" className="w-[65rem]" data={chartData} />
     </div>
   );
 }
